b9bc13460015fc9cc80563d8dce10a35dbf40866
Edu Cortada <cortadaedu@gmail.com>
2019-03-22 17:00:44 +0100

    render cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 
+// CSS
+import 'bulma/css/bulma.css'
+import './style.css'
+
+// Components
+import CardsList from './components/CardsList'
+
 class App extends Component {
+
   render() {
     return (
-      <div>
-        App
-      </div>
+      <section>
+        <CardsList />
+      </section>
     );
   }
 }
---
+++ src/components/CardsList.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+// Components
+import Card from './Card'
+
+class CardsList extends Component {
+
+  state = {
+    activities: []
+  }
+
+  componentDidMount = async () => {
+    const itemsPerPage = 20
+    const offset = 0
+
+    const apiCall = await axios.get(`https://api.musement.com/api/v3/venues/164/activities?limit=${itemsPerPage}&offset=${offset}`)
+    const data = apiCall.data
+
+    this.setState(
+      { activities: data }
+    )
+    console.log(this.state.activities)
+  }
+
+  renderList = () => {
+    return this.state.activities.map(activity => {
+      return <Card key={activity.uuid}activity={activity} />
+    })
+  }
+
+
+  render() {
+    return (
+      <div className="container">
+        <div className="columns is-flex-fullhd">
+          {this.renderList()}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CardsList
